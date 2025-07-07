2c0a2de783a3ba7b4fc5fb8a417a22a4cf19cf99
DuongTran-alt <thescripzom16@gmail.com>
2025-07-07 14:21:43 +0700

    init

---
+++ src/test/java/com/example/plane_ticket/PlaneTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plane_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
